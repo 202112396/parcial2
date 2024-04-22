7edaa402669b4defd7883bfdafed63d1f463f5a7
202112396 <2978391790101@ingenieria.usac.edu.gt>
2024-04-22 00:54:58 -0600

    completo

--- inicio/models.py
+++ inicio/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Usuarios(models.Model):
-    user_id = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=15,null=True,unique=True)
+    firstName = models.CharField(max_length=50,null=True)
+    lastName = models.CharField(max_length=50,null=True)
+    dpi = models.CharField(max_length=13,null=True)
+    age = models.DateField(auto_now=False,auto_now_add=False,null=True)
+    tel = models.CharField(null=True,blank=True)
+    email = models.EmailField(max_length=100,null=True)
+    password = models.CharField(max_length=100,null=True)
--- inicio/views.py
+++ inicio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Usuarios
+from django.db import IntegrityError
 
 # Create your views here.
 def inicio(request):
@@ -12,8 +13,26 @@ def register(request):
 
 def create_user(request):
     New_usr = Usuarios(
-        user_id=request.POST['user_id'], 
-        )
-    New_usr.save()
-    messages.success(request,'¡Creación exitosa! Ya puedes ingresar')
-    return redirect('/inicio/')
+        user_id=request.POST['user_id'],
+        firstName=request.POST['firstName'], 
+        lastName=request.POST['lastName'], 
+        dpi=request.POST['dpi'], 
+        age=request.POST['age'], 
+        tel=request.POST['tel'], 
+        email=request.POST['email'], 
+        password=request.POST['password']
+    )
+    
+    if request.method == 'POST':
+        try:
+            message = "¡Creaste tu usuario con éxito!"
+            return render(request, 'login.html', {'message': message})
+            New_usr.save()
+        except IntegrityError:
+            message = "Este usuario ya existe"
+            return render(request, 'register.html', {'message': message})            
+    return render(request, 'register.html')
+
+    #New_usr.save()
+    #messages.success(request,'¡Creación exitosa! Ya puedes ingresar')
+    #return redirect('/inicio/')
